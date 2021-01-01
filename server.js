5c8a76bb5d3096d7bd3a2ff9770cfdb465ed4915
James Masaviru <jmasaviru@gmail.com>
2020-12-31 23:48:40 -0500

    Adding server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+const express = require("express");
+
+// Express configuration
+const app = express();
+
+// Define the Port
+const PORT = process.env.PORT || 5000;
+
+// Set up Express for data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public")); 
+
+// Routes for API & HTML
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
+
+
+// Set up the Listener
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
